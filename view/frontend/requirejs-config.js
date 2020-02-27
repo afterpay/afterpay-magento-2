95d9119ad8dca9bd2a3644339676c17a903cf911
Max Iengo <max@heavencostumes.com.au>
2020-02-27 15:13:44 +1030

    Update requirejs map configuration

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -8,7 +8,8 @@ var config = {
     map: {
         '*': {
             afterpay:    'https://www-dev.secure-afterpay.com.au/afterpay.js', // @todo change to use dynamic js window.checkoutConfig.payment.afterpay.afterpayJs
-            transparent: 'Magento_Payment/transparent'
+            transparent: 'Magento_Payment/js/transparent',
+            'Magento_Payment/transparent': 'Magento_Payment/js/transparent'
         }
     }
 };
